6ba29f5aafe17f089508bf39bf63cb6623d4ee9a
Pauline <plinekaeorg@gmail.com>
2026-05-02 18:51:19 +0200

    Add CreateProductCommandHandler to Application layer

---
+++ Assignment.Application/Commands/CreateProductCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using Assignment.Application.DTOs;
+using Assignment.Domain.Entities;
+using Assignment.Domain.Interfaces;
+using Assignment.Application.Commands;
+
+namespace Assignment.Application.Features.Products.Commands
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductDto>
+    {
+        private readonly IRepository<Product> _repository;
+        private readonly IMapper _mapper;
+
+        public CreateProductCommandHandler(IRepository<Product> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+    }
+}
