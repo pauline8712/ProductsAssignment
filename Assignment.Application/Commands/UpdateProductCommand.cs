1d4566d6d2eceab31d79ba143395c515b799643e
Pauline <plinekaeorg@gmail.com>
2026-05-02 18:56:24 +0200

    Add UpdateProductCommand to Application layer

---
+++ Assignment.Application/Commands/UpdateProductCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Assignment.Application.Features.Products.Commands
+{
+        public class UpdateProductCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
