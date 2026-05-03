011f83774622973df8a3dc822b70e61d43a955fe
Pauline <plinekaeorg@gmail.com>
2026-05-03 18:39:27 +0200

    Add GetAllProductsQuery with IRequest<IEnumerable<ProductDto>> to fetch all products

---
+++ Assignment.Application/Features/Products/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,12 @@
+﻿using Assignment.Application.DTOs;
+using MediatR;
+using System.Collections.Generic;
+
+//Klassen är tom — vi behöver ingen extra information för att hämta alla produkter
+//IRequest < IEnumerable < ProductDto >> — returnerar en lista av ProductDto till användaren
+namespace Assignment.Application.Features.Products.Queries
+{
+    public class GetAllProductsQuery : IRequest<IEnumerable<ProductDto>>
+    {
+    }
+}
