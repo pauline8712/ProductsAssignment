768bb4e067cdc19ff9faf27e4a030c25efb8aded
Pauline <plinekaeorg@gmail.com>
2026-05-16 18:15:42 +0200

    Added dependency injection extension method to register DbContext and Repository services in Infrastructure layer

---
+++ Assignment.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Assignment.Domain.Entities;
+using Assignment.Domain.Interfaces;
+using Assignment.Infrastructure.Data;
+using Assignment.Infrastructure.Repositories;
+
+namespace Assignment.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IRepository<Product>, Repository<Product>>();
+            services.AddScoped<IRepository<Category>, Repository<Category>>();
+
+            return services;
+        }
+    }
+}
