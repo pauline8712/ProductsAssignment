413c5234e7a6969683e7969088d4b87fba260349
Pauline <plinekaeorg@gmail.com>
2026-05-17 16:26:33 +0200

    Add ProductsController with full CRUD endpoints using MediatR for GetAll, GetById, Create, Update and Delete

---
+++ Assignment.API/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Assignment.Application.Features.Products.Commands;
+using Assignment.Application.Features.Products.Queries;
+
+namespace Assignment.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetAllProductsQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _mediator.Send(new GetProductByIdQuery { Id = id });
+            if (result == null)
+                return NotFound();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateProductCommand command)
+        {
+            command.Id = id;
+            var result = await _mediator.Send(command);
+            if (!result)
+                return NotFound();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _mediator.Send(new DeleteProductCommand { Id = id });
+            if (!result)
+                return NotFound();
+            return NoContent();
+        }
+    }
+}
